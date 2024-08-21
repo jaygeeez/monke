3468643f900270194431dd28378e53c4e5a387b6
Josh Garvida <66766785+jaygeeez@users.noreply.github.com>
2024-08-21 14:24:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -212,56 +212,56 @@ function jumping () {
             . . . f d b b d d c d d f . . . 
             . . . f f f f f f f f f . . . . 
             `,img`
-            . . . . . f f f f f . . . . . . 
-            . . . . f e e e e f . . . . . . 
-            . . . f f f f f f . . . . . . . 
-            f f f e e f . . . c c f . . . . 
-            f d e e e e f . c d d d f . . . 
-            f b f e e e e f f b b d f f . . 
-            f b f e e e e f e e e e f e f . 
-            f d b f f e e e e e e e f e e f 
-            f d d e e e e e d d d f f e e f 
-            f c f f f e f d d d d f f d e f 
-            f d b f f e f d d c d f b d e f 
-            f d d e e f f d c d d d f d e f 
-            . f f f f . f d c d e f f d f . 
-            . . . . . . f d c d e f f f . . 
-            . . . . . . . f c d d f b . . . 
-            . . . . . . . . c c c . . . . . 
+            . . . . . f f f f . . . . . . . 
+            . . . . f e e e f . . . . . . . 
+            f f f f f f f f . . . . . . . . 
+            f d e e e f . . c c f . . . . . 
+            f d f e e e f c d d d f . . . . 
+            f c f e e e e f b b d f f f . . 
+            . f f e e e e f e e e e f e f . 
+            . . f f f e e e e e e e f e e f 
+            . . f e e e e e e d d f f e e f 
+            . . f f f e f e d d d f f d e f 
+            . . f b d e f d d c d f b d e f 
+            . . f d d f f d c d d d f d e f 
+            . . f f f f f d c d e f f d f . 
+            . . f b d f f d c d e f f f . . 
+            . . f d d f f f c d d f b . . . 
+            . . f f f . . . c c c . . . . . 
             `,img`
-            . . . . f f f f f f f f f . . . 
-            . . . f d d c d d b b d f . . . 
-            . . . f d b f d b f f e f . . . 
-            . . . f e f f e f e e e e f . . 
-            . . . f e f f e f e e e e f f . 
-            . . . . f e e e e e e e f f e f 
+            . f f f f f f f . . . f f f . . 
+            f d d c d d d f . . f c d d f . 
+            f d d b d d c f f f f f f b f . 
+            . f e f f e e f f f e e e f . . 
+            . . f f e f e e f e e e e f . . 
+            . . . . f e e e e e e e f f f . 
             . . f f f f f e e e e f . f e f 
-            . f d d d d d e e f f . . f e f 
-            c c c c c d d d e e f c . f e f 
-            c d d d d c d d e e b d c . f f 
-            c d e e d d d d e e b d c . . . 
-            . f f f d f f f e e d d f . . . 
-            . b f f f b f f f f f f . . . . 
-            . . f d d d d e e e f . . . . . 
+            . f c c c d e e e f f . . f e f 
+            c d d d d c d e e e f . . f e f 
+            c d e e d d d d e e f c . f e f 
+            c f f f d f f f e e b d c . f f 
+            . b f f f b f f f f b d c . . . 
+            . c d d d d d d e f f d f . . . 
+            . . f d d d d e e e f f . . . . 
             . . . f e e e e e f . . . . . . 
             . . . . f f f f f . . . . . . . 
             `,img`
             . . . . . c c c . . . . . . . . 
-            . . . b f d d c f . . . . . . . 
-            . . f f f e d c d f . . . . . . 
-            . f d f f e d c d f . f f f f . 
-            f e d f d d d c d f f e e d d f 
-            f e d b f d c d d f e f f b d f 
-            f e d f f d d d d f e f f f c f 
-            f e e f f d d d e e e e e d d f 
-            f e e f e e e e e e e f f b d f 
-            . f e f e e e e f e e e e f b f 
-            . . f f d b b f f e e e e f b f 
-            . . . f d d d c . f e e e e d f 
-            . . . . f c c . . . f e e f f f 
-            . . . . . . . f f f f f f . . . 
-            . . . . . . f e e e e f . . . . 
-            . . . . . . f f f f f . . . . . 
+            . . . c c b f d f . . . . . . . 
+            . . f d d f f e d f . . . f f . 
+            . f d d d f f e d f . . f d d f 
+            f e d d d f d d d f f f e d d f 
+            f e d d d b f d c d e f f b c f 
+            f e e d d f f d d e e e f d d f 
+            f e e e d f f d e e e e e d d f 
+            f e e f f f e e e e e e e c d f 
+            . f f f f e e e f e e e c f f f 
+            . f f b b f f f f e e e d f f . 
+            . f d d d c . . . f e b d f . . 
+            . . f c c . . . . . f f f . . . 
+            . . . . . f f f f f f f f . . . 
+            . . . . . f e e e e f . . . . . 
+            . . . . . . f f f f . . . . . . 
             `,img`
             . . . . . . . f f f f f . . . . 
             . . . . . . f e e e e e f . . . 
@@ -269,84 +269,84 @@ function jumping () {
             . . . . f f f f f f b f f f b . 
             . . . f d d e e f f f d f f f . 
             . . . c d b e e d d d d e e d c 
-            f f . c d b e e d d c d d d d c 
-            f e f . c f e e d d d c c c c c 
-            f e f . . f f e e d d d d d f . 
-            f e f . f e e e e f f f f f . . 
-            f e f f e e e e e e e f . . . . 
-            . f f e e e e f e f f e f . . . 
-            . . f e e e e f e f f e f . . . 
-            . . . f e f f b d f b d f . . . 
-            . . . f d b b d d c d d f . . . 
-            . . . f f f f f f f f f . . . . 
+            . . . c d b e e d d c d d d d c 
+            . . . . c f e e e d d c c c c c 
+            . . . . . f f e e e d d d d f . 
+            . . . . f e e e e f f f f f . . 
+            f f . f e e e e e e f f . . . . 
+            f e . f e e f e e f e e f . . . 
+            f e . f e e e f e e f e e f . . 
+            f e f f e f b b f b d f d b f . 
+            f f f f e b d d f d d f d d f . 
+            . f f f f f f f f f f f f f . . 
             `,img`
-            . . . . . f f f f f . . . . . . 
-            . . . . f e e e e f . . . . . . 
-            . . . f f f f f f . . . . . . . 
-            f f f e e f . . . c c f . . . . 
-            f d e e e e f . c d d d f . . . 
-            f b f e e e e f f b b d f f . . 
-            f b f e e e e f e e e e f e f . 
-            f d b f f e e e e e e e f e e f 
-            f d d e e e e e d d d f f e e f 
-            f c f f f e f d d d d f f d e f 
-            f d b f f e f d d c d f b d e f 
-            f d d e e f f d c d d d f d e f 
-            . f f f f . f d c d e f f d f . 
-            . . . . . . f d c d e f f f . . 
-            . . . . . . . f c d d f b . . . 
-            . . . . . . . . c c c . . . . . 
+            . . . . . f f f f . . . . . . . 
+            . . . . f e e e f . . . . . . . 
+            f f f f f f f f . . . . . . . . 
+            f d e e e f . . c c f . . . . . 
+            f d f e e e f c d d d f . . . . 
+            f c f e e e e f b b d f f f . . 
+            . f f e e e e f e e e e f e f . 
+            . . f f f e e e e e e e f e e f 
+            . . f e e e e e e d d f f e e f 
+            . . f f f e f e d d d f f d e f 
+            . . f b d e f d d c d f b d e f 
+            . . f d d f f d c d d d f d e f 
+            . . f f f f f d c d e f f d f . 
+            . . f b d f f d c d e f f f . . 
+            . . f d d f f f c d d f b . . . 
+            . . f f f . . . c c c . . . . . 
             `,img`
-            . . . . f f f f f f f f f . . . 
-            . . . f d d c d d b b d f . . . 
-            . . . f d b f d b f f e f . . . 
-            . . . f e f f e f e e e e f . . 
-            . . . f e f f e f e e e e f f . 
-            . . . . f e e e e e e e f f e f 
-            . . f f f f f e e e e f . f e f 
-            . f d d d d d e e f f . . f e f 
-            c c c c c d d d e e f c . f e f 
-            c d d d d c d d e e b d c . f f 
-            c d e e d d d d e e b d c . . . 
-            . f f f d f f f e e d d f . . . 
-            . b f f f b f f f f f f . . . . 
+            . . . . . . . . . . f f f f . . 
+            . . . . . . . . . f c d d f . . 
+            f f f f f f f f f f f f e f . . 
+            f d b f d b f e f e e e e f . . 
+            f d d f d d f e f e e e e f f . 
+            . f f f f e e e e e e e f f e f 
+            . f f f f f f e e e e f . f e f 
+            . f d d d d e e e f f c . f e f 
+            c c c c c d d e e e b d c . f f 
+            c d d d d c d d e e b d c . . . 
+            c d e e d d d d e e d d f . . . 
+            . f f f d f f f e e f f . . . . 
+            . b f f f b f f f f f . . . . . 
             . . f d d d d e e e f . . . . . 
             . . . f e e e e e f . . . . . . 
             . . . . f f f f f . . . . . . . 
             `,img`
-            . . . . . c c c . . . . . . . . 
-            . . . b f d d c f . . . . . . . 
-            . . f f f e d c d f . . . . . . 
-            . f d f f e d c d f . f f f f . 
-            f e d f d d d c d f f e e d d f 
-            f e d b f d c d d f e f f b d f 
-            f e d f f d d d d f e f f f c f 
-            f e e f f d d d e e e e e d d f 
-            f e e f e e e e e e e f f b d f 
-            . f e f e e e e f e e e e f b f 
-            . . f f d b b f f e e e e f b f 
-            . . . f d d d c . f e e e e d f 
-            . . . . f c c . . . f e e f f f 
-            . . . . . . . f f f f f f . . . 
-            . . . . . . f e e e e f . . . . 
-            . . . . . . f f f f f . . . . . 
+            . . . . . c c c . . . f f f . . 
+            . . . b f d d c f f f d d f . . 
+            . . f f f e d c d f f d b f . . 
+            . f d f f e d c d f f f f f . . 
+            f e d f d d d c d f f d d f . . 
+            f e d b f d c d d f e d b f . . 
+            f e d f f d d d e f e f f f . . 
+            f e e f f d d e e e e e e f . . 
+            f e e f e e e e e e e f f f . . 
+            . f e f e e e e f e e e e f f . 
+            . . f f f d b b f e e e e f c f 
+            . . . . f d d d c f e e e f d f 
+            . . . . . f c c . . f e e e d f 
+            . . . . . . . . f f f f f f f f 
+            . . . . . . . f e e e f . . . . 
+            . . . . . . . f f f f . . . . . 
             `,img`
             . . . . . . . f f f f f . . . . 
             . . . . . . f e e e e e f . . . 
             . . . . . f e e e d d d d f . . 
-            . . . . f f f f f f b f f f b . 
-            . . . f d d e e f f f d f f f . 
-            . . . c d b e e d d d d e e d c 
-            f f . c d b e e d d c d d d d c 
-            f e f . c f e e d d d c c c c c 
-            f e f . . f f e e d d d d d f . 
-            f e f . f e e e e f f f f f . . 
-            f e f f e e e e e e e f . . . . 
-            . f f e e e e f e f f e f . . . 
-            . . f e e e e f e f f e f . . . 
-            . . . f e f f b d f b d f . . . 
-            . . . f d b b d d c d d f . . . 
-            . . . f f f f f f f f f . . . . 
+            . . . . . f f f f f b f f f b . 
+            . . . . f f e e f f f d f f f . 
+            . . . f d d e e d d d d e e d c 
+            . . . c d b e e d d c d d d d c 
+            f f . c d b e e e d d c c c c c 
+            f e f . c f f e e e d d d d f . 
+            f e f . f e e e e f f f f f f . 
+            f e f f e e e e e e e f f f f . 
+            . f f e e e e f e f d d f d d f 
+            . . f e e e e f e f b d f b d f 
+            . . f e f f f f f f f f f f f f 
+            . . f d d c f . . . . . . . . . 
+            . . f f f f . . . . . . . . . . 
             `,img`
             . . . . . . . f f f f f . . . . 
             . . . . . . f e e e e e f . . . 
@@ -902,12 +902,12 @@ function throwing () {
             . . . c d b e e d d c d d d d c 
             . . . f c f e e d d d f f f f c 
             . . . . f e e e e f f f d b f . 
-            . . . . f e e f f f e f d d f . 
-            . f f . f f f e e e e f f f . . 
-            . f e . f f e e e e f e e f . . 
-            . f e f f f f f f f e e e f f . 
-            . f e f f f b b f e e f d b f . 
-            . f f f f b d d e e f f d d f . 
+            . . . . f e e f f f 8 f d d f . 
+            . f f . f f f 8 8 8 8 f f f . . 
+            . f e . f f 8 8 8 8 f 8 8 f . . 
+            . f e f f f f f f f 8 8 8 f f . 
+            . f e f f f b b f 8 8 f d b f . 
+            . f f f f b d d 8 8 f f d d f . 
             . . f f f f f f f f f f f f f . 
             `],
         100,
